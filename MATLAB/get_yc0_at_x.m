945fb480795dad54b749a5207970d22972630481
juansibecas <juan.sibecas@gmail.com>
2024-03-11 17:38:47 -0300

    moved to scripts folder

--- MATLAB/get_yc0_at_x.m
+++
@@ -1,9 +0,0 @@
-function yc = get_yc0_at_x(profile, x_query)
-
-    xc0 = profile(1,:);
-    yc0 = profile(2,:);
-
-    y1 = interp1(xc0, yc0, x_query, 'previous');
-    y2 = interp1(xc0, yc0, x_query, 'next');
-    yc = max(y1, y2);
-end
